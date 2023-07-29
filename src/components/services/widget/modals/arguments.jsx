4318bd14d027a0c59b60211796d72014c1a2bc0e
wsandst <williamsandstrom99@hotmail.com>
2023-07-29 17:55:22 +0200

    Fix linting issue

--- src/components/services/widget/modals/arguments.jsx
+++ src/components/services/widget/modals/arguments.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import { FaTimes } from "react-icons/fa";
 
